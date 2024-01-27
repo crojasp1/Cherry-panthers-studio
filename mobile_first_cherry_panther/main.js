e0422b6188084254b07044be25b0ad5c132eeadb
crojasp1 <camilorojasprieto@gmail.com>
2024-01-27 15:37:13 -0500

    Left navigation bar JavaScript implemented. Responsive design.

--- mobile_first_cherry_panther/main.js
+++ mobile_first_cherry_panther/main.js
@@ -1,11 +1,18 @@
 const navEmail = document.querySelector('.right-side__email');
 const navDisplayMenu = document.querySelector('.nav__display-menu');
 const navImage = document.querySelector('.navegation-bar__img');
+const menuLeftItem = document.querySelector('.left-side__button');
+const navMobileMenu = document.querySelector('.nav__mobile-menu');
 
 navImage.addEventListener('click', toggleEmailMenu);
 navEmail.addEventListener('click', toggleEmailMenu);
 
+menuLeftItem.addEventListener('click', toggleLeftItem);
+
 function toggleEmailMenu() {
 navDisplayMenu.classList.toggle('inactive');
 }
-console.log("js funciona");
+
+function toggleLeftItem() {
+  navMobileMenu.classList.toggle('inactive');
+}
