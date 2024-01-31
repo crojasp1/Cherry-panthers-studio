81cdba11fd56e24fe5a7afa3693721d37ea087b4
crojasp1 <camilorojasprieto@gmail.com>
2024-01-31 01:42:28 -0500

    Cart product selected menu displayed with the Cart Icon in the right corner. Still working on it. I have to correct some JS functions to avoid everithing stay active when other function is used. Will be done tomorrow. I just wanted to code the aside windows(Menus) first.

--- mobile_first_cherry_panther/main.js
+++ mobile_first_cherry_panther/main.js
@@ -1,18 +1,26 @@
 const navEmail = document.querySelector('.right-side__email');
 const navDisplayMenu = document.querySelector('.nav__display-menu');
+const cartproducts = document.querySelector('.selected-product');
+
+
 const navImage = document.querySelector('.navegation-bar__img');
 const menuLeftItem = document.querySelector('.left-side__button');
 const navMobileMenu = document.querySelector('.nav__mobile-menu');
 
-navImage.addEventListener('click', toggleEmailMenu);
+navImage.addEventListener('click', toggleSelectedItemsMenu);
+
 navEmail.addEventListener('click', toggleEmailMenu);
 
 menuLeftItem.addEventListener('click', toggleLeftItem);
 
 function toggleEmailMenu() {
-navDisplayMenu.classList.toggle('inactive');
+  navDisplayMenu.classList.toggle('inactive');
 }
 
 function toggleLeftItem() {
   navMobileMenu.classList.toggle('inactive');
+}
+
+function toggleSelectedItemsMenu() {
+  cartproducts.classList.toggle('inactive');
 }
