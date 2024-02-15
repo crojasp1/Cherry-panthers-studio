882fb78346af8cb1212fcf21e499ac42f0707744
crojasp1 <camilorojasprieto@gmail.com>
2024-02-15 15:26:17 -0500

    Product detail buttons design changed. Product detail displayed by JS, still working on it, the close button didnt worked as I set in html because the way I did it its not recognized. Some info add in shopping site header.

--- mobile_first_cherry_panther/main.js
+++ mobile_first_cherry_panther/main.js
@@ -10,13 +10,15 @@ const navMobileMenu = document.querySelector('.nav__mobile-menu');
 const productDetail = document.querySelector('.product-detail ');
 
 
+
+
 navImage.addEventListener('click', toggleSelectedItemsMenu);
 
 navEmail.addEventListener('click', toggleEmailMenu);
 
 menuLeftItem.addEventListener('click', toggleLeftItem);
 
-closeArrow.addEventListener('click', closeProductDetail);
+/*closeArrow.addEventListener('click', closeProductDetail);*/
 
 
 
@@ -52,6 +54,8 @@ function openProductDetail() {
   navMobileMenu.classList.add('inactive');
   cartproducts.classList.add('inactive');
 
+  
+
 }
 function closeProductDetail() {
   productDetail.classList.add('inactive');
@@ -63,6 +67,7 @@ const productos = [{nombre:"Mustang", tipo:"Deportivo", valor: 160000, descripci
                   {nombre:"GTR", tipo:"Clasico", valor: 190000, descripcion: "Auto clasico de dos puertas super potente", image: './assets/images/autoNegroPeque.jpg'},
                   {nombre:"GTR", tipo:"Clasico", valor: 190000, descripcion: "Auto clasico de dos puertas super potente", image: './assets/images/autoNegroPeque.jpg'},
                   {nombre:"Ferrari", tipo:"Elegante", valor: 210000, descripcion: "Auto de gran valoracion con busnas prestaciones", image: './assets/images/autoRojoPeque.jpg'}];
+
 
 
                 /*<div class="main-header__card-container">
@@ -116,3 +121,88 @@ for (producto of productos){
 
   mainHeaderCardContainer.appendChild(cardContainerProduct);
 }
+
+/*Se crea con la misma lista de objetos la vista de los detalles del producto
+
+<div class="product-detail__container">
+  <img src="./assets/images/autoNaranjaPeque.jpg" alt="Imagen del producto">
+  <span class="product-detail__close"><i class="fa-solid fa-circle-xmark"></i></span>
+  <p>Tipo de producto</p>
+  <h2>Nombre del producto</h2>
+  <div class="product-detail__specifications">
+    <p>$ 69000</p>
+    <p>Pedir por wapp <i class="fa-brands fa-whatsapp"></i></p>
+  </div>
+  <p>Informacion de los productos</p>
+
+  <div class="product-detail__boton-compra">
+
+    <div class="product-detail__añadir-articulo">
+      <p>$ 69000</p>
+      <input type="submit" value="Añadir articulo">
+  </div>
+</div>
+</div>
+*/
+
+
+    const productDetailContainer = document.createElement('div');
+    productDetailContainer.classList.add('product-detail__container');
+
+    const productDetailImage = document.createElement('img');
+    productDetailImage.setAttribute('src', producto.image);
+    productDetailImage.setAttribute('alt', 'Imagen del producto');
+
+    const productDetailClose = document.createElement('span');
+    productDetailClose.classList.add('product-detail__close');
+
+    const closeIcon = document.createElement('i');
+    /*closeIcon.classList.add('fa-solid fa-circle-xmark');*/
+
+    productDetailClose.append(closeIcon);
+
+    const tipoDeProducto = document.createElement('p');
+    tipoDeProducto.innerText = producto.tipo;
+
+    const nombreProducto = document.createElement('h2');
+    nombreProducto.innerText = producto.nombre;
+
+    const productDetailSpecifications = document.createElement('div');
+    productDetailSpecifications.classList.add('product-detail__specifications');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$ ' + producto.valor;
+    const pedirWhatsApp = document.createElement('p');
+    pedirWhatsApp.innerText = 'Pedir por WhatsApp';
+    const whatsappIcon = document.createElement('i');
+    /*whatsappIcon.classList.add('fa-brands fa-whatsapp')*/
+
+    pedirWhatsApp.append(whatsappIcon);
+
+    productDetailSpecifications.append(pedirWhatsApp, productPrice);
+
+
+    const productInfo = document.createElement('p');
+    productInfo.innerText = producto.descripcion;
+
+   const botonCompra = document.createElement('div');
+   botonCompra.classList.add('product-detail__boton-compra');
+
+   const añadirArticulo = document.createElement('div');
+   añadirArticulo.classList.add('product-detail__añadir-articulo');
+
+   const precio = document.createElement('p');
+   precio.innerText = '$ ' + producto.valor;
+
+   const botonAñadir = document.createElement('input');
+   botonAñadir.setAttribute('type', 'submit');
+   botonAñadir.setAttribute('value', 'Añadir articulo');
+
+   añadirArticulo.append(precio, botonAñadir);
+
+   botonCompra.append(añadirArticulo);
+
+   productDetailContainer.append(productDetailImage, productDetailClose, tipoDeProducto, nombreProducto, productDetailSpecifications, productInfo, botonCompra);
+
+  productDetail.append(productDetailContainer);
+
